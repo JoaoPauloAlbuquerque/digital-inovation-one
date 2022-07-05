ee5d489bb144816664441ad1140eb54204d5b0a4
João Paulo <jp123albuquerque321santos@gmail.com>
2022-07-05 20:17:29 -0300

    Criando a classe Conta

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -1,4 +1,16 @@
 package one.digitalinovation.digionebank
 
 class Pessoa {
+    var nome: String = "pedro"
+    private set // Define privado para a propriedade set da variavel acima
+    var cpf: String = "123.456.789-00"
+    private set
+    constructor()
+
+    public fun getNomeAndCpf() = "$nome + $cpf"
+}
+
+fun main() {
+    val pessoa = Pessoa()
+    println(pessoa.getNomeAndCpf())
 }
